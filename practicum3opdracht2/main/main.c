a5db3700e7a6d64862990aed596743e3fdaf1fc6
Rik <rik.vos01@gmail.com>
2021-03-07 14:03:33 +0100

    [Add] practicum3opdracht2 opzet

--- practicum3opdracht2/main/main.c
+++ practicum3opdracht2/main/main.c
@@ -2,11 +2,36 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "driver/gpio.h"
+#include "semaphore.h"
 
+sem_t writerSemaphore,readerSemaphore;
+
+
+void writerTask(void *pvParameters){
+    
+    for(;;){
+
+    }
+}
+
+void readerTask(void *pvParameters){
+
+    for(;;){
+        //xSemaphoreTake(readerSemaphore,2000/portTICK_PERIOD_MS);
+        printf("Taking semaphore");
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        //xSemaphoreGive(readerSemaphore);
+        printf("Giving semaphore");
+    }
+}
 
 void app_main()
 {
+    xSemaphoreCreateBinary();
+
+    xTaskCreate(readerTask,"Reader",2048,NULL,3,NULL);
+    xTaskCreate(readerTask,"Reader",2048,NULL,3,NULL);
+
     ets_timer_init();
 	
     for(;;){
